4ba71cc69b17d0b4b0fe2803137d7f0826acee32
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-24 18:49:47 -0300

    ff

---
+++ flowlogs/flowlogs.tf
@@ -0,0 +1,35 @@
+############################
+# NSG existente
+############################
+data "azurerm_network_security_group" "nsg" {
+  name                = "nsg-traffic-lab"
+  resource_group_name = "rg-traffic-lab"
+}
+
+############################
+# Storage Account existente
+############################
+data "azurerm_storage_account" "sa" {
+  name                = var.storage_account_name
+  resource_group_name = "rg-traffic-lab"
+}
+
+############################
+# Flow Logs
+############################
+resource "azurerm_network_watcher_flow_log" "flowlog" {
+  name                 = "flowlog-nsg"
+  network_watcher_name = "NetworkWatcher_brazilsouth"
+  resource_group_name  = "NetworkWatcherRG"
+
+  target_resource_id = data.azurerm_network_security_group.nsg.id
+  storage_account_id = data.azurerm_storage_account.sa.id
+
+  enabled = true
+  version = 2
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+}
---
+++ flowlogs/variables.tf
@@ -0,0 +1,4 @@
+variable "storage_account_name" {
+  description = "Nome da Storage Account existente"
+  type        = string
+}
