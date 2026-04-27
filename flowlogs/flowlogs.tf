d1165ad572d920acabc0c7677e42af60dedeba08
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-27 18:07:35 -0300

    teste2

--- flowlogs/flowlogs.tf
+++ flowlogs/flowlogs.tf
@@ -1,26 +1,49 @@
-############################
+########################################
+# Provider
+########################################
+provider "azurerm" {
+  features {}
+}
+
+########################################
+# Network Watcher existente
+########################################
+data "azurerm_network_watcher" "nw" {
+  name                = "NetworkWatcher_brazilsouth"
+  resource_group_name = "NetworkWatcherRG"
+}
+
+########################################
 # NSG existente
-############################
+########################################
 data "azurerm_network_security_group" "nsg" {
   name                = "nsg-traffic-lab"
   resource_group_name = "rg-traffic-lab"
 }
 
-############################
+########################################
 # Storage Account existente
-############################
+########################################
 data "azurerm_storage_account" "sa" {
   name                = "stotrafficflow1035"
   resource_group_name = "rg-traffic-lab"
 }
 
-############################
+########################################
+# Log Analytics Workspace existente
+########################################
+data "azurerm_log_analytics_workspace" "law" {
+  name                = "SEU_WORKSPACE_NOME"
+  resource_group_name = "SEU_WORKSPACE_RG"
+}
+
+########################################
 # Flow Logs
-############################
+########################################
 resource "azurerm_network_watcher_flow_log" "flowlog" {
   name                 = "flowlog-nsg"
-  network_watcher_name = "NetworkWatcher_brazilsouth"
-  resource_group_name  = "NetworkWatcherRG"
+  network_watcher_name = data.azurerm_network_watcher.nw.name
+  resource_group_name  = data.azurerm_network_watcher.nw.resource_group_name
 
   target_resource_id = data.azurerm_network_security_group.nsg.id
   storage_account_id = data.azurerm_storage_account.sa.id
@@ -31,5 +54,13 @@ resource "azurerm_network_watcher_flow_log" "flowlog" {
   retention_policy {
     enabled = true
     days    = 7
+  }
+
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = data.azurerm_log_analytics_workspace.law.workspace_id
+    workspace_region      = data.azurerm_log_analytics_workspace.law.location
+    workspace_resource_id = data.azurerm_log_analytics_workspace.law.id
+    interval_in_minutes   = 10
   }
 }
--- flowlogs/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "storage_account_name" {
-  description = "stotrafficflow1035"
-  type        = string
-}
