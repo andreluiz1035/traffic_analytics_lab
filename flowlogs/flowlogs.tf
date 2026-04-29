eb5b1d7fe1d364415596e1f42e88d3a20c300123
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-29 09:22:00 -0300

    teste-a

--- flowlogs/flowlogs.tf
+++ flowlogs/flowlogs.tf
@@ -30,11 +30,15 @@ data "azurerm_storage_account" "sa" {
 }
 
 ########################################
-# Log Analytics Workspace existente
+# Log Analytics Workspace 
 ########################################
-data "azurerm_log_analytics_workspace" "law" {
-  name                = "SEU_WORKSPACE_NOME"
-  resource_group_name = "SEU_WORKSPACE_RG"
+
+resource  "azurerm_log_analytics_workspace" "law" {
+  name                = "law-traffic-lab"
+  resource_group_name = "rg-traffic-lab"
+  location            = "Brazil South"
+  sku               = "PerGB2018"
+  retention_in_days   = 30
 }
 
 ########################################
@@ -58,9 +62,9 @@ resource "azurerm_network_watcher_flow_log" "flowlog" {
 
   traffic_analytics {
     enabled               = true
-    workspace_id          = data.azurerm_log_analytics_workspace.law.workspace_id
-    workspace_region      = data.azurerm_log_analytics_workspace.law.location
-    workspace_resource_id = data.azurerm_log_analytics_workspace.law.id
+    workspace_id          = azurerm_log_analytics_workspace.law.workspace_id
+    workspace_region      = azurerm_log_analytics_workspace.law.location
+    workspace_resource_id = azurerm_log_analytics_workspace.law.id
     interval_in_minutes   = 10
   }
 }
